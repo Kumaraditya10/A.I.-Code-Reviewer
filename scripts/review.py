823aa644ec4b59ac9bb67dcf4c70f68cd516f3b7
Aditya Kumar <aditya10112005@gmail.com>
2026-03-24 17:09:36 +0530

    test-5

--- scripts/review.py
+++ scripts/review.py
@@ -1,4 +1,6 @@
 import subprocess
+from google import genai
+import os
 
 def getDiff():
     diff = subprocess.check_output(["git", "show"], text=True)
@@ -6,4 +8,17 @@ def getDiff():
 
 
 
-print(getDiff())
+client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def main():
+    diff = getDiff()
+    prompt = f"Review the following code changes and provide feedback:\n\n{diff}"
+    responce = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents=prompt
+    )
+    print("Code Review Feedback")
+    print(responce.text)
+    
+    
+main()
